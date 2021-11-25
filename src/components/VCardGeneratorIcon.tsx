d8778cc8e1411386b1a06ea3ee51972fe161a597
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-25 16:25:04 -0300

    Added VCardGeneratorIcon reusable component

---
+++ src/components/VCardGeneratorIcon.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from "react";
+
+export default function VCardGeneratorIcon(): ReactElement {
+  return (
+    <div>
+      <h1>vCard generator</h1>
+    </div>
+  );
+}
