a636fd31ccb703add519f771b3cc8b21aee44fa5
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-25 02:16:28 -0300

    Template for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/">
+          <Route path="" element={<h2>Hello from "/"!</h2>} />
+          <Route path="/cards">
+            <Route path="" element={<h2>Hello from "/cards"!</h2>} />
+            <Route path=":id" element={<h2>Hello from "/cards/:id"!</h2>} />
+            <Route path="new" element={<h2>Hello from "/cards/new"!</h2>} />
+          </Route>
+        </Route>
+        <Route path="*" element={<h2>Page not found!</h2>} />
+      </Routes>
     </div>
   );
 }
