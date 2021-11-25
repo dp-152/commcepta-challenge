b06c17ef5226675a013c2dd48e91fbad06650d66
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-25 20:52:44 -0300

    Added disabled prop to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "../css/components/Button.css";
 
@@ -9,16 +9,19 @@ interface Props {
   size: "small" | "large";
   target: string;
   icon?: string;
+  isDisabled?: boolean;
 }
 
 export default function Button(props: Props): ReactElement {
+  const location = useLocation();
   return (
-    <Link to={props.target}>
+    <Link to={props.isDisabled ? location.pathname : props.target}>
       <div
         className={
           "Button " +
           `Button-theme-${props.theme} ` +
-          `Button-size-${props.size}`
+          `Button-size-${props.size} ` +
+          (props.isDisabled ? "disabled" : "")
         }
       >
         {props.icon ? <img src={props.icon} alt={props.icon} /> : ""}
