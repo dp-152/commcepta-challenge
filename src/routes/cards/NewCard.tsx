31c622ee00cee7f562368aa66429562ee2c1a6ae
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-26 19:05:44 -0300

    Reworked NewCard to accomodate for API model

--- src/routes/cards/NewCard.tsx
+++ src/routes/cards/NewCard.tsx
@@ -1,24 +1,38 @@
-import React, { ReactElement, useCallback } from "react";
-import { useNavigate } from "react-router";
+import React, { ReactElement, useCallback, useState } from "react";
 
 import BackButton from "../../components/BackButton";
+import CardContainer from "../../components/CardContainer";
 import NewCardForm from "../../components/NewCardForm";
 import VCardGeneratorIcon from "../../components/VCardGeneratorIcon";
 import { CardData } from "../../data/CardData";
+import repo from "../../repository/DefaultCommceptersRepository";
 
 export default function NewCard(): ReactElement {
-  const nav = useNavigate();
-  const submitHandler = useCallback(
-    (data: CardData) => {
-      // TODO: push to database, retrieve new ID
-      console.log(data);
-      const newID = 1;
-      nav("/cards/" + newID);
-    },
-    [nav],
-  );
+  const [userData, setUserData] = useState({} as CardData);
+  const [qrCode, setQrCode] = useState("");
+  const [shouldRenderCard, setShouldRenderCard] = useState(false);
 
-  return (
+  const submitHandler = useCallback((data: CardData) => {
+    data.company = "Commcepta";
+    setUserData(data);
+    setShouldRenderCard(true);
+
+    repo.makeQRCode(data).then(img => {
+      setQrCode(img);
+    });
+  }, []);
+
+  return shouldRenderCard ? (
+    <div>
+      <BackButton />
+      <CardContainer
+        firstName={userData.firstName}
+        lastName={userData.lastName}
+        position={userData.position}
+        b64QrCode={qrCode}
+      />
+    </div>
+  ) : (
     <div>
       <VCardGeneratorIcon />
       <BackButton />
