ca1bc8fc83577d3e158db643059dcb59090a6f24
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-26 04:44:37 -0300

    Implemented base logic for NewCard component

--- src/routes/cards/NewCard.tsx
+++ src/routes/cards/NewCard.tsx
@@ -1,9 +1,21 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useCallback } from "react";
+
+import BackButton from "../../components/BackButton";
+import NewCardForm from "../../components/NewCardForm";
+import VCardGeneratorIcon from "../../components/VCardGeneratorIcon";
+import { CardData } from "../../data/CardData";
 
 export default function NewCard(): ReactElement {
+  const submitHandler = useCallback((data: CardData) => {
+    // TODO: push to database, retrieve new ID
+    console.log(data);
+  }, []);
+
   return (
     <div>
-      <h1>Hello, World! from NewCard.tsx @ "/cards/new"</h1>
+      <VCardGeneratorIcon />
+      <BackButton />
+      <NewCardForm submitHandler={submitHandler} />
     </div>
   );
 }
