50f6501059fb63cd3ec47f247b00be216b846298
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-26 20:39:57 -0300

    Moved existing CSS files into appropriate directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 
 import Main from "./routes/Main";
 import Cards from "./routes/cards/Cards";
