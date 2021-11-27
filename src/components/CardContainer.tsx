1e2bf87dd8c738698d6614ccba52db4e69ee828d
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-27 20:53:22 -0300

    Reworked CardContainer to accept new layouts

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,4 +1,6 @@
 import React, { ReactElement } from "react";
+
+import "../css/components/CardContainer.css";
 import logoIcon from "../assets/icon commcepta.png";
 import logoText from "../assets/logo commcepta.png";
 
@@ -16,13 +18,23 @@ export default function CardContainer({
   b64QrCode,
 }: Props): ReactElement {
   return (
-    <div>
-      <span>{firstName}</span>
-      <span>{lastName}</span>
-      <span>{position}</span>
-      <img src={logoIcon} alt="Logo icon" />
-      <img src={`data:image/png;base64,${b64QrCode}`} alt="QR Code" />
-      <img src={logoText} alt="Logo commcepta" />
+    <div className="CardContainer">
+      <div className="CardContainer-top">
+        <div className="CardContainer-details">
+          <span className="CardContainer-name">{firstName}</span>
+          <span className="CardContainer-name">{lastName}</span>
+          <span className="CardContainer-position">{position}</span>
+        </div>
+        <div className="CardContainer-icon">
+          <img src={logoIcon} alt="Logo icon" />
+        </div>
+      </div>
+      <div className="CardContainer-qrcode">
+        <img src={`data:image/png;base64,${b64QrCode}`} alt="QR Code" />
+      </div>
+      <div className="CardContainer-logo">
+        <img src={logoText} alt="Logo commcepta" />
+      </div>
     </div>
   );
 }
