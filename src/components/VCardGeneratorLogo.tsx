7dd7fb6896385cb01827b7af6ed39776fea51c40
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-27 14:12:11 -0300

    Added theme support to vCard logo

--- src/components/VCardGeneratorLogo.tsx
+++ src/components/VCardGeneratorLogo.tsx
@@ -1,12 +1,16 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useContext } from "react";
+import ThemeContext from "../contexts/ThemeContext";
 
 import "../css/components/VCardGeneratorLogo.css";
 
 export default function VCardGeneratorLogo(): ReactElement {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="VCardGeneratorLogo">
-      <span className="VCardGeneratorLogo-topText">vCard</span>
-      <span className="VCardGeneratorLogo-bottomText">generator</span>
+    <div className={`VCardGeneratorLogo theme-${theme}`}>
+      <span className={`VCardGeneratorLogo-topText theme-${theme}`}>vCard</span>
+      <span className={`VCardGeneratorLogo-bottomText theme-${theme}`}>
+        generator
+      </span>
     </div>
   );
 }
