999ebab168eda82ccabd0e487865dad9030fbab6
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-26 22:29:40 -0300

    Reformatted components inside Container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,12 +1,28 @@
 import React, { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
 
+import "./css/Container.css";
+import logo from "./assets/logo commcepta.png";
+
 export default function Container(): ReactElement {
   return (
-    <div>
-      <h1>TOP</h1>
-      <Outlet />
-      <p>BOTTOM</p>
+    <div className="Container">
+      <header className="Container-header">
+        <img
+          src={logo}
+          alt="Logo"
+          className="Container-header-logo"
+          width="228"
+        />
+      </header>
+      <main className="Container-main">
+        <Outlet />
+      </main>
+      <footer className="Container-footer">
+        <span className="Container-footer-copyrightnotice">
+          © 2021. Docummented as Commcepter
+        </span>
+      </footer>
     </div>
   );
 }
