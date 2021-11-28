db665c60159d6920aa3d94fc84b2fd296e93c47d
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-27 21:18:20 -0300

    Render with CardPage class

--- src/routes/cards/NewCard.tsx
+++ src/routes/cards/NewCard.tsx
@@ -15,7 +15,7 @@ import { CardData } from "../../data/CardData";
 import repo from "../../repository/DefaultCommceptersRepository";
 
 export default function NewCard(): ReactElement {
-  const { setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
   const [userData, setUserData] = useState({} as CardData);
   const [qrCode, setQrCode] = useState("");
   const [shouldRenderCard, setShouldRenderCard] = useState(false);
@@ -35,15 +35,14 @@ export default function NewCard(): ReactElement {
   }, []);
 
   return shouldRenderCard ? (
-    <div>
-      <BackButton />
+    <section className={`CardPage-wrapper theme-${theme}`}>
       <CardContainer
         firstName={userData.firstName}
         lastName={userData.lastName}
         position={userData.position}
         b64QrCode={qrCode}
       />
-    </div>
+    </section>
   ) : (
     <div>
       <VCardGeneratorLogo />
